3b5cd34c3759f366e55171941c512e51acdfbbb3
RodryAlexander <rodrygoleu7@gmail.com>
2025-12-14 17:23:16 -0500

    feat: use public environments

--- apps/frontend/src/lib/apis.ts
+++ apps/frontend/src/lib/apis.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.BACKEND_URL || 'http://localhost:3000',
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3000',
   withCredentials: true,
 });
